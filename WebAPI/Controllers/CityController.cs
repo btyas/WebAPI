3fc175ea4572cad65a741fe34124ba8b20fa8e6a
btyas <terdayetyassine@gmail.com>
2021-03-13 10:39:39 +0100

    Add project files.

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly DataContext dc;
+
+        public CityController(DataContext dc)
+        {
+            this.dc = dc;
+        }
+
+
+        [HttpGet]
+       public async Task<IActionResult> GetCities()
+        {
+            var cities = await dc.Cities.ToListAsync();
+            return Ok(cities);
+        }
+        // Post api/city/add?citynaame
+        [HttpPost("add")]
+        public async Task<ActionResult> AddCity( string cityName)
+        {
+            City city = new City();
+            city.Name = cityName;
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+    }
+}
